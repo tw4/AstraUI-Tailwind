f590bce599498cab20fcdad782557b2ba62c7974
6fy <88425310+6fy@users.noreply.github.com>
2023-06-18 14:53:33 +0300

    update: button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,7 +5,7 @@ export interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {}
 const Button: FC<ButtonProps> = ({ children, className, ...rest }) => (
   <>
     <button
-      className={`bg-[#1890FF] text-white px-[15px] py-[4px]  rounded-[2px] hover:bg-[#40A9FF]  ${className}`}
+      className={`flex flex-row items-center bg-[#1890FF] text-white px-[15px] py-[4px] rounded-[2px] hover:bg-[#40A9FF]  ${className}`}
       {...rest}>
       {children}
     </button>
